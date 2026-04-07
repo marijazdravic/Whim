4537e121cae43559e583d666e1abcff3cc2a9927
Marija <marijazdravic@gmail.com>
2026-04-07 22:07:51 +0200

    Initial commit - Add project structure

---
+++ Whim/Whim.swift
@@ -0,0 +1,9 @@
+//
+//  Whim.swift
+//  Whim
+//
+//  Created by Marija Zdravic on 07.04.2026..
+//
+
+import Foundation
+
---
+++ WhimTests/WhimTests.swift
@@ -0,0 +1,13 @@
+//
+//  WhimTests.swift
+//  WhimTests
+//
+//  Created by Marija Zdravic on 07.04.2026..
+//
+
+import Testing
+import Whim
+
+struct WhimTests {
+   
+}

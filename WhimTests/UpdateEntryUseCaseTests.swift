b98695774980456ac62609fe915b85f7e51df8ca
Marija <marijazdravic@gmail.com>
2026-04-20 17:47:55 +0200

    Add apply delivers not found error when entry does not exist

--- WhimTests/UpdateEntryUseCaseTests.swift
+++ WhimTests/UpdateEntryUseCaseTests.swift
@@ -7,14 +7,20 @@ enum EntryUpdate {
 }
 
 final class EntryUpdater {
+    enum Error: Swift.Error {
+        case notFound
+    }
+
     let store: EntryStore
-    
+
     init(store: EntryStore) {
         self.store = store
     }
-    
+
     func apply(_ update: EntryUpdate, to id: UUID) throws {
-        _ = try store.retrieve(by: id)
+        guard try store.retrieve(by: id) != nil else {
+            throw Error.notFound
+        }
     }
 }
 
@@ -22,22 +28,33 @@ struct EntryUpdaterTests {
     @Test
     func init_doesNotRequestStoreMessages() {
         let (_, store) = makeSUT()
-        
+
         #expect(store.receivedMessages.isEmpty)
     }
-    
+
     @Test
     func apply_requestsStoreToRetrieveEntryByID() throws {
         let (sut, store) = makeSUT()
         let id = UUID()
-        
+
         try? sut.apply(.setText("Updated text"), to: id)
-        
+
         #expect(store.receivedMessages == [.retrieve(id)])
     }
-    
+
+    @Test
+    func apply_throwsNotFoundWhenEntryDoesNotExist() {
+        let (sut, _) = makeSUT()
+
+        let id = UUID()
+
+        #expect(throws: EntryUpdater.Error.notFound) {
+            try sut.apply(.setText(anyText()), to: id)
+        }
+    }
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> (sut: EntryUpdater, store: EntryStoreSpy) {
         let store = EntryStoreSpy()
         let sut = EntryUpdater(store: store)
